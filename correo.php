4f197296d38e5345f68be3c79c8a4c4e30d191c1
Eduardo Hernandez <chg14edu@gmail.com>
2015-06-26 14:51:07 -0500

    Versión 2.0.1
    Corrige errores en el envío de e-mails.
    Usa la última versión de font awesomwe (4.3)
    El formulario cambia los íconos de Bootstrap a Font awesome.

--- correo.php
+++ correo.php
@@ -1,49 +1,27 @@
 <!DOCTYPE html>
 <html>
-	<head>
+<head>
 	    <meta charset="UTF-8"/>
 	    <title>Código rápido</title>
 	    <link rel="shortcut icon" href="assets/img/favicon.png" />
 	</head>
-	<body>
+<body>
 	<?php
 	if(extract($_POST)){
 		//SMTP needs accurate times, and the PHP time zone MUST be set
 		//This should be done in your php.ini, but this is how to do it if you don't have access to that
 		date_default_timezone_set("America/Mexico_City");
 		require 'phpmailer/PHPMailerAutoload.php';
-		
 		//Create a new PHPMailer instance
 		$mail = new PHPMailer();
-		//Tell PHPMailer to use SMTP
-		$mail->isSMTP();
-		//Enable SMTP debugging
-		// 0 = off (for production use)
-		// 1 = client messages
-		// 2 = client and server messages
-		$mail->SMTPDebug = 0;
-		//Se define la codificación
-		$mail->CharSet="UTF-8";
-		//Ask for HTML-friendly debug output
-		$mail->Debugoutput = 'html';
-		//Set the hostname of the mail server
-		$mail->Host="smtp.gmail.com";
-		//Set the SMTP port number - likely to be 25, 465 or 587
-		$mail->Port=587;
-		//Set the SMTP Secure
-		$mail->SMTPSecure = 'tls';
-		//Whether to use SMTP authentication
-		$mail->SMTPAuth = true;
-		//Username to use for SMTP authentication
-		$mail->Username = "chg14edu@gmail.com";
-		//Password to use for SMTP authentication
-		$mail->Password = "141747laLO";
+		//Set character encoding
+		$mail->CharSet = 'UTF-8';
 		//Set who the message is to be sent from
 		$mail->setFrom("$email", "$name");
 		//Set an alternative reply-to address
 		$mail->addReplyTo("$email", "$name");
 		//Set who the message is to be sent to
-		$mail->addAddress('codrapmexico@gmail.com', 'Eduardo Hernández');
+		$mail->addAddress('codrapmexico@gmail.com', 'Código rápido');
 		//Set the subject line
 		$mail->Subject = 'Código rápido';
 		//Read an HTML message body from an external file, convert referenced images to embedded,
@@ -55,9 +33,13 @@
 		if (!$mail->send()) {
 		    echo "<div class='alert alert-danger'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>Lo sentimos $name ocurrio un error al envíarse el mensaje.</div>";
 		} else {
-		    echo "<div class='alert alert-success'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>Gracias por comunicarse con nosotros $name.</div>";
+			if ($email == "mlpc.b2@outlook.com" || $email == "marie93lpcon1008@gmail.com") {
+				echo "<div class='alert alert-success'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>Gracias mi amor por toda tu ayuda</div>";
+			} else {
+		    	echo "<div class='alert alert-success'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>Gracias por comunicarse con nosotros $name.</div>";
+			}
 		}
 	}
-	?>
-	</body>
+?>
+</body>
 </html>
